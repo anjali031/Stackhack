d4cacb8e991d5c62a89a9259e6f09bce1c39b8ff
Anjali Dubey <anjalidubey9844@gmail.com>
2020-05-30 17:23:07 +0530

    changes made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { ToDoCreateComponent } from './to-do/to-do-create/to-do-create.component';
 import { ToDoReadComponent } from './to-do/to-do-read/to-do-read.component';
 
 
 
 const routes: Routes = [
-  { path: 'signup', component: SignupComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'todo', component: ToDoCreateComponent},
+
   { path: 'todo/read', component: ToDoReadComponent},
   { path: '' , redirectTo: '/todo/read', pathMatch : 'full'}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 import { UserService } from './shared/user.service';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ToDoCreateComponent } from './to-do/to-do-create/to-do-create.component';
 import { ToDoReadComponent } from './to-do/to-do-read/to-do-read.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
     NavbarComponent,
-    ToDoCreateComponent,
     ToDoReadComponent
   ],
   imports: [
